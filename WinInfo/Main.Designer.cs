507d1c64bc0cea53e4548d38e01941e15d53c49f
Bananamilk452 <bananamilk452@gmail.com>
2019-06-04 23:45:23 +0900

    Dashboard work complete, new FlatUI

--- WinInfo/Main.Designer.cs
+++ WinInfo/Main.Designer.cs
@@ -31,8 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.TabControl = new System.Windows.Forms.TabControl();
             this.dashboard = new System.Windows.Forms.TabPage();
-            this.OSVersion = new System.Windows.Forms.Label();
             this.CPUPage = new System.Windows.Forms.TabPage();
+            this.Log = new System.Windows.Forms.TabPage();
             this.H1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.MinButton = new System.Windows.Forms.Button();
@@ -40,20 +40,33 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.ProgramVersion = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TabControl1 = new FlatUI.FlatTabControl();
+            this.tab_dash = new System.Windows.Forms.TabPage();
+            this.OSVersion = new System.Windows.Forms.Label();
+            this.NetworkDevice = new System.Windows.Forms.Label();
+            this.SystemName = new System.Windows.Forms.Label();
+            this.Processor = new System.Windows.Forms.Label();
+            this.SoundDevice = new System.Windows.Forms.Label();
+            this.GPU = new System.Windows.Forms.Label();
+            this.InstalledRAM = new System.Windows.Forms.Label();
+            this.tab_log = new System.Windows.Forms.TabPage();
+            this.logbox = new System.Windows.Forms.ListBox();
             this.TabControl.SuspendLayout();
-            this.dashboard.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.TabControl1.SuspendLayout();
+            this.tab_dash.SuspendLayout();
+            this.tab_log.SuspendLayout();
             this.SuspendLayout();
             // 
             // TabControl
             // 
             this.TabControl.Controls.Add(this.dashboard);
             this.TabControl.Controls.Add(this.CPUPage);
-            this.TabControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TabControl.Controls.Add(this.Log);
             this.TabControl.Font = new System.Drawing.Font("맑은 고딕", 13F);
-            this.TabControl.Location = new System.Drawing.Point(0, 83);
+            this.TabControl.Location = new System.Drawing.Point(584, 223);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
             this.TabControl.Size = new System.Drawing.Size(811, 399);
@@ -61,7 +74,7 @@
             // 
             // dashboard
             // 
-            this.dashboard.Controls.Add(this.OSVersion);
+            this.dashboard.BackColor = System.Drawing.Color.White;
             this.dashboard.Font = new System.Drawing.Font("맑은 고딕", 9F);
             this.dashboard.Location = new System.Drawing.Point(4, 32);
             this.dashboard.Name = "dashboard";
@@ -69,17 +82,6 @@
             this.dashboard.Size = new System.Drawing.Size(803, 363);
             this.dashboard.TabIndex = 0;
             this.dashboard.Text = "Dashboard";
-            this.dashboard.UseVisualStyleBackColor = true;
-            // 
-            // OSVersion
-            // 
-            this.OSVersion.AutoSize = true;
-            this.OSVersion.Font = new System.Drawing.Font("맑은 고딕", 10F);
-            this.OSVersion.Location = new System.Drawing.Point(3, 12);
-            this.OSVersion.Name = "OSVersion";
-            this.OSVersion.Size = new System.Drawing.Size(97, 19);
-            this.OSVersion.TabIndex = 1;
-            this.OSVersion.Text = "윈도우 버전 : ";
             // 
             // CPUPage
             // 
@@ -90,6 +92,16 @@
             this.CPUPage.TabIndex = 1;
             this.CPUPage.Text = "CPU";
             this.CPUPage.UseVisualStyleBackColor = true;
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(4, 32);
+            this.Log.Name = "Log";
+            this.Log.Padding = new System.Windows.Forms.Padding(3);
+            this.Log.Size = new System.Drawing.Size(803, 363);
+            this.Log.TabIndex = 2;
+            this.Log.Text = "Log";
+            this.Log.UseVisualStyleBackColor = true;
             // 
             // H1
             // 
@@ -147,13 +159,15 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.TabControl1);
             this.panel2.Controls.Add(this.ProgramVersion);
             this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.TabControl);
             this.panel2.Controls.Add(this.H1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 10);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(811, 73);
+            this.panel2.Size = new System.Drawing.Size(811, 472);
             this.panel2.TabIndex = 3;
             this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
             // 
@@ -175,6 +189,143 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // TabControl1
+            // 
+            this.TabControl1.ActiveColor = System.Drawing.Color.DodgerBlue;
+            this.TabControl1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.TabControl1.Controls.Add(this.tab_dash);
+            this.TabControl1.Controls.Add(this.tab_log);
+            this.TabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TabControl1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.TabControl1.ItemSize = new System.Drawing.Size(120, 40);
+            this.TabControl1.Location = new System.Drawing.Point(0, 67);
+            this.TabControl1.Name = "TabControl1";
+            this.TabControl1.SelectedIndex = 0;
+            this.TabControl1.Size = new System.Drawing.Size(811, 405);
+            this.TabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.TabControl1.TabIndex = 8;
+            // 
+            // tab_dash
+            // 
+            this.tab_dash.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(60)))), ((int)(((byte)(75)))));
+            this.tab_dash.Controls.Add(this.OSVersion);
+            this.tab_dash.Controls.Add(this.NetworkDevice);
+            this.tab_dash.Controls.Add(this.SystemName);
+            this.tab_dash.Controls.Add(this.Processor);
+            this.tab_dash.Controls.Add(this.SoundDevice);
+            this.tab_dash.Controls.Add(this.GPU);
+            this.tab_dash.Controls.Add(this.InstalledRAM);
+            this.tab_dash.Location = new System.Drawing.Point(4, 44);
+            this.tab_dash.Name = "tab_dash";
+            this.tab_dash.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_dash.Size = new System.Drawing.Size(803, 357);
+            this.tab_dash.TabIndex = 0;
+            this.tab_dash.Text = "대시보드";
+            // 
+            // OSVersion
+            // 
+            this.OSVersion.AutoSize = true;
+            this.OSVersion.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.OSVersion.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OSVersion.Location = new System.Drawing.Point(7, 82);
+            this.OSVersion.Name = "OSVersion";
+            this.OSVersion.Size = new System.Drawing.Size(97, 19);
+            this.OSVersion.TabIndex = 1;
+            this.OSVersion.Text = "윈도우 버전 : ";
+            // 
+            // NetworkDevice
+            // 
+            this.NetworkDevice.AutoSize = true;
+            this.NetworkDevice.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.NetworkDevice.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.NetworkDevice.Location = new System.Drawing.Point(7, 154);
+            this.NetworkDevice.Name = "NetworkDevice";
+            this.NetworkDevice.Size = new System.Drawing.Size(166, 19);
+            this.NetworkDevice.TabIndex = 7;
+            this.NetworkDevice.Text = "네트워크 장치 (연결됨) : ";
+            // 
+            // SystemName
+            // 
+            this.SystemName.AutoSize = true;
+            this.SystemName.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.SystemName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SystemName.Location = new System.Drawing.Point(7, 10);
+            this.SystemName.Name = "SystemName";
+            this.SystemName.Size = new System.Drawing.Size(97, 19);
+            this.SystemName.TabIndex = 2;
+            this.SystemName.Text = "시스템 이름 : ";
+            // 
+            // Processor
+            // 
+            this.Processor.AutoSize = true;
+            this.Processor.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.Processor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Processor.Location = new System.Drawing.Point(7, 34);
+            this.Processor.Name = "Processor";
+            this.Processor.Size = new System.Drawing.Size(78, 19);
+            this.Processor.TabIndex = 3;
+            this.Processor.Text = "프로세서 : ";
+            // 
+            // SoundDevice
+            // 
+            this.SoundDevice.AutoSize = true;
+            this.SoundDevice.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.SoundDevice.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SoundDevice.Location = new System.Drawing.Point(7, 130);
+            this.SoundDevice.Name = "SoundDevice";
+            this.SoundDevice.Size = new System.Drawing.Size(97, 19);
+            this.SoundDevice.TabIndex = 6;
+            this.SoundDevice.Text = "사운드 장치 : ";
+            // 
+            // GPU
+            // 
+            this.GPU.AutoSize = true;
+            this.GPU.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.GPU.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GPU.Location = new System.Drawing.Point(7, 106);
+            this.GPU.Name = "GPU";
+            this.GPU.Size = new System.Drawing.Size(133, 19);
+            this.GPU.TabIndex = 5;
+            this.GPU.Text = "GPU (그래픽카드) : ";
+            // 
+            // InstalledRAM
+            // 
+            this.InstalledRAM.AutoSize = true;
+            this.InstalledRAM.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.InstalledRAM.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.InstalledRAM.Location = new System.Drawing.Point(7, 58);
+            this.InstalledRAM.Name = "InstalledRAM";
+            this.InstalledRAM.Size = new System.Drawing.Size(155, 19);
+            this.InstalledRAM.TabIndex = 4;
+            this.InstalledRAM.Text = "설치된 메모리 (RAM) : ";
+            // 
+            // tab_log
+            // 
+            this.tab_log.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.tab_log.Controls.Add(this.logbox);
+            this.tab_log.Location = new System.Drawing.Point(4, 44);
+            this.tab_log.Name = "tab_log";
+            this.tab_log.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_log.Size = new System.Drawing.Size(803, 357);
+            this.tab_log.TabIndex = 1;
+            this.tab_log.Text = "로그";
+            // 
+            // logbox
+            // 
+            this.logbox.BackColor = System.Drawing.Color.DarkGray;
+            this.logbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logbox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.logbox.Enabled = false;
+            this.logbox.Font = new System.Drawing.Font("맑은 고딕", 11F);
+            this.logbox.FormattingEnabled = true;
+            this.logbox.ItemHeight = 19;
+            this.logbox.Location = new System.Drawing.Point(3, 3);
+            this.logbox.Name = "logbox";
+            this.logbox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.logbox.Size = new System.Drawing.Size(797, 351);
+            this.logbox.TabIndex = 0;
+            this.logbox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.logbox_DrawItem);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -183,19 +334,21 @@
             this.ClientSize = new System.Drawing.Size(811, 482);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.TabControl);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("맑은 고딕", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "WinInfo";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.TabControl.ResumeLayout(false);
-            this.dashboard.ResumeLayout(false);
-            this.dashboard.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.TabControl1.ResumeLayout(false);
+            this.tab_dash.ResumeLayout(false);
+            this.tab_dash.PerformLayout();
+            this.tab_log.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -213,6 +366,17 @@
         private System.Windows.Forms.Label ProgramVersion;
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button MinButton;
+        private System.Windows.Forms.Label SystemName;
+        private System.Windows.Forms.Label Processor;
+        private System.Windows.Forms.Label InstalledRAM;
+        private System.Windows.Forms.Label GPU;
+        private System.Windows.Forms.Label SoundDevice;
+        private System.Windows.Forms.Label NetworkDevice;
+        private System.Windows.Forms.TabPage Log;
+        private FlatUI.FlatTabControl TabControl1;
+        private System.Windows.Forms.TabPage tab_dash;
+        private System.Windows.Forms.TabPage tab_log;
+        private System.Windows.Forms.ListBox logbox;
     }
 }
 
--- WinInfo/Main.cs
+++ WinInfo/Main.cs
@@ -2,6 +2,10 @@
 using System.Management;
 using System.Windows.Forms;
 using System.Reflection;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Net.NetworkInformation;
+using FlatUI;
 
 namespace WinInfo
 {
@@ -12,10 +16,12 @@ namespace WinInfo
             InitializeComponent();
         }
 
-        #region 창 드래그 코드
+        #region 창 드래그, 크기 조정 코드
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
+        private const int cGrip = 16;
+        private const int cCaption = 32;
 
         [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
@@ -40,16 +46,92 @@ namespace WinInfo
             }
         }
 
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
         #endregion
+
+        
+
+        public void AddLog(string logtext, Color color)
+        {
+            ColoredItem coloredItem = new ColoredItem { Color = color, Text = DateTime.Now.ToLongTimeString().ToString() + " : " + logtext };
+            logbox.Items.Add(coloredItem);
+        }
+
+        class ColoredItem
+        {
+            public string Text;
+            public Color Color;
+        };
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // 코드 최적화 하기
+            // 코드 최적화 하기
+            // 코드 최적화 하기
+            AddLog("프로그램 정상 로딩", Color.Blue);
+            AddLog("버전 : " + Assembly.GetExecutingAssembly().GetName().Version.ToString(), Color.Blue);
+
+            string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.ToString();
             ProgramVersion.Text = "version " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + "Win32_OperatingSystem");
-            foreach (ManagementObject share in searcher.Get())
-            {
-                OSVersion.Text = OSVersion.Text + share["Caption"].ToString() + " " + share["Version"].ToString();
+            ManagementObjectSearcher searcher;
+
+            ManagementObjectSearcher GetWMI(string MainKey) {
+               return searcher = new ManagementObjectSearcher("SELECT * FROM " + MainKey);
             }
+
+            foreach (ManagementObject share in GetWMI("Win32_OperatingSystem").Get())
+            {
+                OSVersion.Text = OSVersion.Text + share["Caption"].ToString() + " " + share["Version"].ToString() + " (" + share["OSArchitecture"].ToString() + ")";
+                InstalledRAM.Text = InstalledRAM.Text + Int32.Parse(share["TotalVisibleMemorySize"].ToString()) / 1000000 + "GB";
+            }
+            foreach (ManagementObject share in GetWMI("Win32_UserAccount").Get())
+            {
+                SystemName.Text = SystemName.Text + share["Fullname"].ToString();
+            }
+            SystemName.Text = SystemName.Text + " (" + username + ")";
+            foreach (ManagementObject share in GetWMI("Win32_Processor").Get())
+            {
+                Processor.Text = Processor.Text + share["Name"].ToString() + " , " + share["NumberOfCores"].ToString() + " 코어, " + share["ThreadCount"].ToString() + " 논리 프로세서";
+            }
+            foreach (ManagementObject share in GetWMI("Win32_VideoController").Get())
+            {
+                GPU.Text = GPU.Text + share["Name"].ToString() + " (" + share["VideoModeDescription"].ToString().Split('x')[0] + "x" + share["VideoModeDescription"].ToString().Split('x')[1] + "x " + share["CurrentRefreshRate"] + "hz), " + Math.Floor(UInt32.Parse(share["AdapterRAM"].ToString()) / 1e+9) + "GB 메모리";
+            }
+            foreach (ManagementObject share in GetWMI("Win32_SoundDevice").Get())
+            {
+                SoundDevice.Text = SoundDevice.Text + share["Caption"].ToString() + ", ";
+            }
+            var networks = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface share in networks)
+            {
+                if (share.OperationalStatus.ToString() == "Up")
+                {
+                    NetworkDevice.Text = NetworkDevice.Text + share.Name + ", ";
+                }
+            }
+            // 코드 최적화 하기
+            // 코드 최적화 하기
+            // 코드 최적화 하기
         }
 
         private void MinButton_Click(object sender, EventArgs e)
@@ -60,6 +142,17 @@ namespace WinInfo
         private void CloseButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void logbox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            ColoredItem item = logbox.Items[e.Index] as ColoredItem;
+
+            if (item != null)
+            {
+                e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                e.Graphics.DrawString(item.Text, e.Font, new SolidBrush(item.Color), e.Bounds, StringFormat.GenericDefault);
+            }
         }
     }
 }
